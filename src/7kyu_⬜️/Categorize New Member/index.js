316646762434f0d2f421e063edd087a65d3b7403
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-06-18 21:04:04 -0700

    add 7kyu categorize new member

---
+++ src/7kyu_⬜️/Categorize New Member/index.js
@@ -0,0 +1,10 @@
+// function openOrSenior(data) {
+//   return data.map(([age, handicap]) =>
+//     age > 54 && handicap > 7 ? "Senior" : "Open"
+//   );
+// }
+
+const openOrSenior = (data) =>
+  data.map(([age, handicap]) => (age > 54 && handicap > 7 ? "Senior" : "Open"));
+
+module.exports = openOrSenior;
