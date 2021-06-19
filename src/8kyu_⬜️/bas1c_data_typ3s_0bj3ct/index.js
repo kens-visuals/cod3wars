faf346160b9758bec07b8c3c9056ac2f3a0d2de7
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-06-19 14:53:56 -0700

    bas1c_data_typ3s_0bj3ct

---
+++ src/8kyu_⬜️/bas1c_data_typ3s_0bj3ct/index.js
@@ -0,0 +1,7 @@
+// function animal(obj) {
+//   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+// }
+
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+
+module.exports = animal;
