51e525f6abd5971fd6088f5aed4dbf80dd99a69b
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-06-19 11:20:19 -0700

    8kyu bas1c_data_typ3s_str1ng

--- src/8kyu_⬜️/sum_arrays/index.js
+++
@@ -1,8 +0,0 @@
-// function sum(numbers) {
-//   return numbers.reduce((acc, mov) => acc + mov, 0);
-// }
-
-const sum = (numbers) =>
-  numbers.length === 0 ? 0 : numbers.reduce((acc, cur) => acc + cur);
-
-module.exports = sum;
--- src/8kyu_⬜️/sum_arrays/index.test.js
+++
@@ -1,9 +0,0 @@
-const sum = require("./index");
-
-test("fixed tests", () => {
-  expect(sum([])).toBe(0);
-});
-
-test("fixed tests", () => {
-  expect(sum([1, 5.2, 4, 0, -1])).toBe(9.2);
-});
