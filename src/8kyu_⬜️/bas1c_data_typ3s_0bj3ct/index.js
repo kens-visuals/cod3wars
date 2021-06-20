e6146c297ade7f606d61c81d9e405d544f6ceb7e
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-06-20 14:46:59 -0700

    delete old files

--- src/8kyu_⬜️/bas1c_data_typ3s_0bj3ct/index.js
+++
@@ -1,7 +0,0 @@
-// function animal(obj) {
-//   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
-// }
-
-const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
-
-module.exports = animal;
