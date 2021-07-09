5740de46d6640ab17560b0eb2efcefb1c4f1b2ca
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-07-09 16:09:53 -0700

    add 5kyu exercises

---
+++ src/5kyu_🟨/mult1_l1n3_task_f1zz_buzz/README.md
@@ -0,0 +1,12 @@
+# Description
+
+You need to complete the FizzBuzz function:
+
+- if `n` is divisible by 15, return `'fizzbuzz'`
+- if `n` is divisible by 5, return `'buzz'`
+- if `n` is divisible by 3, return `'fizz'`
+- otherwise, return `n` as a number
+
+where `n` is a positive integer.
+
+Requirement: Every line must have at most `3` characters, and total number of lines must be less than `25`.
---
+++ src/5kyu_🟨/mult1_l1n3_task_f1zz_buzz/index.js
@@ -0,0 +1,48 @@
+// COPY THEN UNCOMMENT
+
+// f=
+// n=>
+// n
+// %15
+// ==0
+// ?
+// `f\
+// iz\
+// zb\
+// uz\
+// z`:
+// n%5
+// ==0
+// ?
+// `b\
+// uz\
+// z`:
+// n%3
+// ==0
+// ?
+// `f\
+// iz\
+// z`:
+// n;
+
+// JUST IN CASE YOU NEED THE TESTS
+
+// test("fixed tests", () => {
+//     expect(f(3)).toBe('fizz);
+// });
+
+// test("fixed tests", () => {
+//     expect(f(5)).toBe('buzz);
+// });
+
+// test("fixed tests", () => {
+//     expect(f(15)).toBe('fizzbuzz);
+// });
+
+// test("fixed tests", () => {
+//     expect(f(8)).toBe(8);
+// });
+
+// test("fixed tests", () => {
+//     expect(f(28)).toBe(28);
+// });
